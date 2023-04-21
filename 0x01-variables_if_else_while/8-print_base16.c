a481d25edc6c44cc2e02a3a8206c055c65084a20
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-04-21 08:16:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints numbers 0 to 9 and lowercase alphabets
+ * between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
