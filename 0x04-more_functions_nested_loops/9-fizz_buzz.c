e3f9905f8f28435eb449a98a44cd3fc00717ea53
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-04-28 17:14:25 -0700

    Nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the numbers from 1 to 100 followed
- * by new line, for multiples of three prints Fizz instaed of the 
+ * by new line, for multiples of three prints Fizz instaed of the
  * number and for multiples of five prints Buzz
  * Return: Always 0 (Success)
  */
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 !=0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
 		}
