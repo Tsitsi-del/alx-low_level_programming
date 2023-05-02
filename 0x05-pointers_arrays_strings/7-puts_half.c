1f994b349a51369bf6076f098e07d97a66b6d4fd
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-05-02 12:43:08 -0700

    Update pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,34 @@
+#include "main.h"
 
+/**
+ * puts_half - prints half a string
+ * @str: holds a string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+	if (count % 2 == 0)
+	{
+		for (i = count / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (count % 2 != 0)
+	{
+		for (i = (count - 1) / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	_putchar('\n');
+}
