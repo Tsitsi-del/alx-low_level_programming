80ad72cdedc244f780541b5f0c2322ebc2f0c593
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-05-02 12:22:55 -0700

    Update pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,15 @@
 void puts2(char *str)
 {
 	int i;
+	int j = 0;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i];
-				i++;
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
