4a2ecd66cd2f8c6972a9976e6b999520cfb2ec2a
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-05-02 04:32:10 -0700

    Pointers, arrays and string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the string in revrse
+ * @s: holds the character string
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	for (i = count; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
