efd995fe9558594539ced025c53a50fa833caade
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-05-02 04:41:00 -0700

    Pointers, arrays amd string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_rev - prints the string in revrse
+ * print_rev - prints the
+ * string in revrse
  * @s: holds the character string
  * Return: 0
  */
@@ -12,12 +13,8 @@ void print_rev(char *s)
 	int count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
 		count++;
-	}
 	for (i = count; i >= 0; i--)
-	{
 		_putchar(s[i]);
-	}
 	_putchar('\n');
 }
