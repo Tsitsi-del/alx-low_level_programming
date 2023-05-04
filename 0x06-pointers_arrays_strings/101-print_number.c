0f34208db1fbb3865111e6b02674afd03615dccd
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-05-04 07:13:25 -0700

    update pointers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * @n: number
+ * Return: n
+ */
+
+void print_number(int n)
+	_putchar(n)
+{
+	int main()
+	{
+		int i = 0;
+
+		while (i < n)
+		{
+			_putchar(n);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *n);
 char *cap_string(char *str);
 char *leet(char *);
+void print_number(int n);
 
 #endif
