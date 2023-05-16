6d9b324943302c2ab5dd0d102f599bf06719b182
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-05-16 16:17:06 -0700

    Update malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,8 +15,12 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	ptr = malloc(sizeof(char) * size);
-	if (ptr == 0 || ptr == NULL)
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = (char *) malloc(sizeof(char) * size);
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
