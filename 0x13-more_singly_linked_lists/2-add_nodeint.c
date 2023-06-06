cfdb017300c40a7fd9a8bad80d5752363d6d4b76
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-06-06 01:42:02 -0700

    Update number 2 on removing space before tab line 10

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,7 +7,7 @@
  * @head: pointer to the address of the head of the lists
  * @n: value of the new node
  * Return: NULL, if function fails
- * 	Otherwise - address of the new node
+ *	Otherwise - address of the new node
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
