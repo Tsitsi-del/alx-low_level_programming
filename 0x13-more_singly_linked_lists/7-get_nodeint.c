a4d754ae9afbf60559009cc9408a45582ea3573c
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-06-06 01:52:47 -0700

    Update question 7 and 8 removed spaces on comments

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to the head of list
  * @index: index of the node to locate
  * Return: if node does not exist - NULL
- * 	Otherwise - the located node
+ *	Otherwise - the located node
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,7 +4,7 @@
  * sum_listint - calculates the sum of data in list
  * @head: pointer to the head of the list
  * Return: if list is empty - 0
- * 	Otherwise - the sum of the list
+ *	Otherwise - the sum of the list
  */
 
 int sum_listint(listint_t *head)
