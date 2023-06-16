ae22a6eaf0f9ef34af5c6f93dd5cfc6521adfdc9
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-06-16 06:43:31 -0700

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,8 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 	file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	byts = write(file_d, text_content, len);
