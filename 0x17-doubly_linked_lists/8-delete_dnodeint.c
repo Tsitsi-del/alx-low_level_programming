82f7f338a3db0e868545627b961aa88148e54a81
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-07-21 07:13:02 -0700

    Update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - Function returns number of nodes in a list
+ * @h: pointer to list
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num_nodes = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		num_nodes++;
+		h = h->next; }
+
+	return (num_nodes);
+}
+/**
  * delete_dnodeint_at_index - Function that deletes node in list
  * @head: pointer to the list
  * @index: index of node to delete
@@ -24,13 +44,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		}
 		tem->next = NULL;
 		free(tem);
-		return (1);
-	}
+		return (1); }
 	while (x < index)
 	{
 		tem = tem->next;
-		x++;
-	}
+		x++; }
 	tem->prev->next = tem->next;
 	if (tem->next)
 		tem->next->prev = tem->prev;
